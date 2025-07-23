283b2bd8ab06d98b92c2734fc3572990a6feffbe
karinbarazarte <karinbarazarte@gmail.com>
2025-07-23 12:19:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,6 @@ def remesas():
     return render_template('remesas.html', paises=paises, bancos=bancos, colaboradores=colaboradores)
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
