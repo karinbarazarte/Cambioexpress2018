b3a0aa99b1a44834a85b324ff92bec63174ee64e
karinbarazarte <karinbarazarte@gmail.com>
2025-07-24 09:29:56 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "<h1>Dashboard funcionando en Render</h1>"
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/remesas')
 def remesas():
-    return "<h2>Página de Remesas funcionando</h2>"
+    return render_template('remesas.html')
 
-@app.route('/bancos')
-def bancos():
-    return "<h2>Módulo de Bancos funcionando</h2>"
+@app.route('/bancos/venezuela')
+def bancos_venezuela():
+    return render_template('bancos_venezuela.html')
+
+@app.route('/bancos/peru')
+def bancos_peru():
+    return render_template('bancos_peru.html')
+
+@app.route('/bancos/chile')
+def bancos_chile():
+    return render_template('bancos_chile.html')
+
+@app.route('/bancos/colombia')
+def bancos_colombia():
+    return render_template('bancos_colombia.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
